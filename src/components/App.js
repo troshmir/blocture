5136025475c6c7b048dd05cbde4c848c37c6c97c
Miridon Troshupa <troshmir@students.zhaw.ch>
2019-10-24 10:26:41 +0200

    d

--- src/components/App.js
+++ src/components/App.js
@@ -13,7 +13,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Dapp University
+           Blocture
           </a>
         </nav>
         <div className="container-fluid mt-5">
