324ce217afa5a2b77c3367f4bd714c7bd30e00e3
Miridon Troshupa <troshmir@students.zhaw.ch>
2019-10-24 10:40:30 +0200

    safe

--- src/components/App.js
+++ src/components/App.js
@@ -27,7 +27,7 @@ class App extends Component {
                 >
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Dapp University Starter Kit</h1>
+                <h1>Who uses your image?</h1>
                 <p>
                   Edit <code>src/components/App.js</code> and save to reload.
                 </p>
