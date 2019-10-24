f03598d9082e24bd90e7c733c4fc0d39ef7f74c6
Miridon Troshupa <troshmir@students.zhaw.ch>
2019-10-24 11:56:37 +0200

    Form

--- src/components/App.js
+++ src/components/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
           <a
             className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
+            href="https://github.com/troshmir/blocture"
             target="_blank"
             rel="noopener noreferrer"
           >
@@ -19,26 +19,22 @@ class App extends Component {
         <div className="container-fluid mt-5">
           <div className="row">
             <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
+              <div className="content mr-auto ml-auto"> <br/> <br/>
                 <a
-                  href="http://www.dappuniversity.com/bootcamp"
+                  href="https://github.com/troshmir/blocture"
                   target="_blank"
                   rel="noopener noreferrer"
                 >
                   <img src={logo} className="App-logo" alt="logo" />
                 </a>
-                <h1>Who uses your image?</h1>
-                <p>
-                  Edit <code>src/components/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  LEARN BLOCKCHAIN <u><b>NOW! </b></u>
-                </a>
+                <h1>Wer benutzt ihr Bild?</h1> <br/> <br/>
+            
+             <h2>Bild hochladen</h2>
+                <form>
+                  <input type='file'/>
+                  <input type='submit'/>
+                </form>
+                 
               </div>
             </main>
           </div>
